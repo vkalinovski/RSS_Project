47f6e6447c81ee76dbe047d9aa2527ccf27f13a0
kalinovski vladimir <120317370+vkalinovski@users.noreply.github.com>
2025-04-28 22:32:42 +0300

    Update rss_feeds.py

--- rss_feeds.py
+++ rss_feeds.py
@@ -10,12 +10,19 @@ RSS_FEEDS = {
     "Global Times":         "https://www.globaltimes.cn/rss/home.xml",
     "South China Morning Post": "https://www.scmp.com/rss/91/feed",
     "People's Daily (EN)":  "http://en.people.cn/rss/politics.xml",
+    "China News Service":   "http://www.ecns.cn/rss/rss.xml",
+    "Sixth Tone":           "https://www.sixthtone.com/rss",
+    "Caixin Global":        "https://www.caixinglobal.com/rss.xml",
 
     # Россия
     "TASS":                 "https://tass.com/rss/v2.xml?departments=politics",
     "RIA Novosti":          "https://ria.ru/export/rss2/politics/index.xml",
     "RBC Politics":         "https://rssexport.rbc.ru/rbc/logical/footer/news.rss",
     "Meduza EN":            "https://meduza.io/rss/en/all",
+    "Kommersant":           "https://www.kommersant.ru/RSS/news.xml",
+    "Gazeta.Ru Politics":   "https://www.gazeta.ru/export/rss/politics.xml",
+    "Lenta.Ru Politics":    "https://lenta.ru/rss/news/politics/",
+    "RT English":           "https://www.rt.com/rss/news/",
 
     # Международные
     "Reuters World":        "http://feeds.reuters.com/Reuters/worldNews",
@@ -37,5 +44,18 @@ RSS_FEEDS = {
     "Deutsche Welle":       "https://rss.dw.com/xml/rss-en-politics",
     "France24 Politics":    "https://www.france24.com/en/rss/politics.xml",
     "Al Jazeera":           "https://www.aljazeera.com/xml/rss/all.xml?language=en",
-    "AFP Top News":         "https://www.afp.com/rss.xml?category=world"
+    "AFP Top News":         "https://www.afp.com/rss.xml?category=world",
+    "DW Politics":          "https://rss.dw.com/rdf/rss-en-pol",
+    "AP International":     "https://apnews.com/hub/international-news?outputType=xml",
+    "Foreign Policy":       "https://foreignpolicy.com/feed/",
+    "Straits Times":        "https://www.straitstimes.com/news/world/rss.xml",
+    "Japan Times":          "https://www.japantimes.co.jp/feed/topstories/",
+    "Times of India":       "https://timesofindia.indiatimes.com/rssfeeds/296589292.cms",
+    "Haaretz International":"https://www.haaretz.com/srv/feed/feedsIntlFrontPage.xml",
+    "Sydney Morning Herald":"https://www.smh.com.au/rss/world.xml",
+    "Toronto Star World":   "https://www.thestar.com/content/feeds.world.rss",
+    "Brazilian Report":     "https://brazilian.report/feed/",
+    "Africa News":          "https://www.africanews.com/feed/",
+    "Middle East Eye":      "https://www.middleeasteye.net/rss.xml",
+    "ASEAN Today":          "https://www.aseantoday.com/feed/"
 }
