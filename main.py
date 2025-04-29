2b23b7fc126e2e967ad75231b18f74a4a2454aed
kalinovski vladimir <120317370+vkalinovski@users.noreply.github.com>
2025-04-29 11:28:59 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from database import create_database, remove_duplicates
-
-if __name__ == "__main__":
-    create_database()
-    remove_duplicates()
-    print("База готова — можно запускать import/update.")
